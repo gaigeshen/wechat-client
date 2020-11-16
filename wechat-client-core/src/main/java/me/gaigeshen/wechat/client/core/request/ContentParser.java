d8f9cd155682751c447d75d74aa7eebfd3c69e81
gaigeshen <gaigeshen@qq.com>
2020-11-17 00:43:43 +0800

    请求内容数据转换器重新设计

--- wechat-client-core/src/main/java/me/gaigeshen/wechat/client/core/request/ContentParser.java
+++ wechat-client-core/src/main/java/me/gaigeshen/wechat/client/core/request/ContentParser.java
@@ -1,10 +1,12 @@
 package me.gaigeshen.wechat.client.core.request;
 
+import me.gaigeshen.wechat.client.core.http.RequestContent;
+
 /**
  * @author gaigeshen
  */
-public interface ContentParser<C> {
+public interface ContentParser {
 
-  C parse(Content<?> content) throws ContentParserException;
+  RequestContent parse(Content<?> content) throws ContentParserException;
 
 }
